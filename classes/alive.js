2dc6b48bfd35078749d85e3fe28ccfb2eac3cf85
nomean42 <ygreknomean@gmail.com>
2015-08-24 13:50:46 +0600

    test

---
+++ classes/alive.js
@@ -0,0 +1,48 @@
+/**
+ *
+ * @param {{
+ * firstName: string
+ * secondName: string
+ * thirdName: string
+ * id: number
+ * whoIs: Alive.WhoIs
+ * }} paramsAlive
+ * @constructor
+ */
+Alive = function(paramsAlive) {
+	this.firstName = paramsAlive.firstName;
+	this.secondName = paramsAlive.secondName;
+	this.thirdName = paramsAlive.thirdName;
+	this.id = paramsAlive.id;
+	this.whoIs = paramsAlive.whoIs;
+
+};
+
+
+/**
+ *
+ * @param paramsAlive
+ */
+Alive.prototype.createAlive = function(paramsAlive) {
+	var newParamsAlive = {};
+	var currentId = idEnum.length + 1;
+	newParamsAlive.firstName = paramsAlive.firstName;
+	newParamsAlive.secondName = paramsAlive.secondName;
+	newParamsAlive.thirdName = paramsAlive.thirdName;
+	newParamsAlive.whoIs = paramsAlive.whoIs;
+	newParamsAlive.id = currentId;
+	idEnum[idEnum.length] = currentId;
+	var currentAlive = new Alive(newParamsAlive);
+	aliveDataBase[currentId] = currentAlive;
+};
+
+/**
+ *
+ * @type {{STUDENT: string, TEACHER: string, ADMIN: string}}
+ * @enum {string}
+ */
+Alive.WhoIs =  {
+	STUDENT: 'student',
+	TEACHER: 'teacher',
+	ADMIN: 'admin'
+}
