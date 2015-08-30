8363bf5930b58cf8ea5c9f1df3827211657b8bd3
nomean42 <ygreknomean@gmail.com>
2015-08-30 15:09:04 +0600

    Add base of DataBase

---
+++ service/data-base.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * @constructor
+ * @implements IDataBase
+ */
+DataBase = function() {};
+
+
+/**
+ * @inheritDoc
+ */
+DataBase.prototype.set = function(member) {};
+
+
+/**
+ *
+ * @inheritDoc
+ */
+DataBase.prototype.get = function(property, value) {};
