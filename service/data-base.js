1edb153caaea99e14094fa1f1f3da97348696978
nomean42 <ygreknomean@gmail.com>
2015-09-04 02:08:50 +0600

    Add todo

--- service/data-base.js
+++ service/data-base.js
@@ -12,10 +12,10 @@ DataBase = function() {};
 DataBase.prototype.set = function(member) {
 	member.setId();
 	memberDataBase.push(member);
-	return member._id;
+	return member._id; //todo 'нельзя обращаться так к непубличным свойствам. нужен публичный метод member.getId()'
 };
 
-
+//todo
 /**
  *
  * @inheritDoc
