6ad9a748ca837f6e197d70400d134352a6958788
nomean42 <ygreknomean@gmail.com>
2015-09-09 22:53:07 +0600

    Typing

--- classes/admin.js
+++ classes/admin.js
@@ -1,9 +1,19 @@
 /**
  *
  * @param {Member.Params} baseParams
+ * @param {{
+ * 		id: string
+ * }} adminParams
  * @constructor
  */
-Admin = function(baseParams) {
+Admin = function(baseParams, adminParams) {
+	this._id = adminParams.id;
 	Member.call(this, baseParams, Member.Type.ADMIN);
 };
 Admin.prototype = Object.create(Member.prototype);
+
+/**
+ * @type {string}
+ * @protected
+ */
+Admin.prototype._id;
