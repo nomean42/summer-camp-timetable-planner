48ff8907bd64eebba20fc49f7dade04445d21df4
nomean42 <ygreknomean@gmail.com>
2015-10-17 14:41:47 +0600

    Add users route

---
+++ app/service/api-server/routes/lib/users.js
@@ -0,0 +1,28 @@
+var express = require('express');
+
+
+/**
+ * @return {express.Router}
+ */
+var users = function() {
+	var router = express.Router();
+
+	router.get('/', function(req, res, next) {
+		next();
+		return res.send('USERS!');
+	});
+	router.get('/id/:id', function(req, res, next) {
+		var id = req.params.id;
+		return res.send('id:' + id);
+	});
+
+
+	router.get('/count', function(req, res, next) {
+		return res.send('count!');
+	});
+
+	return router;
+};
+
+
+module.exports = users;
