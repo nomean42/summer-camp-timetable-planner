5c5c1304f50c737e6ebabb50dd87776e6a0b7daa
nomean42 <ygreknomean@gmail.com>
2015-10-20 01:30:56 +0600

    refactor

--- run.js
+++
@@ -1,9 +0,0 @@
-var idEnum = [];
-var vasya = {};
-var aliveDataBase = {};
-for (i=1; i <= 10;) {
-	Alive.prototype.createAlive ({firstName: 'kostya'});
-	++i;
-};
-console.log(idEnum);
-console.log(aliveDataBase[7]);
