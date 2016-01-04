e31246d007b8137cb5398603e7d905e5f1007417
nomean42 <ygreknomean@gmail.com>
2016-01-04 14:30:05 +0600

    Add get user array by param with check

--- app/service/api-server/service/route-manager/routes/lib/users.js
+++ app/service/api-server/service/route-manager/routes/lib/users.js
@@ -10,12 +10,26 @@ var users = function(database) {
 
 	router.all('/id/:id', function(req, res, next) {
 		var id = req.params.id;
-		//TODO get user by id
 		var result = database.get(Database.Type.MEMBER, 'id', id);
 		return res.send(result);
 	});
 
 	//TODO get user array by params
+	//TODO linter conflicts
+	router.all('/param', function(req, res, next) {
+		var isRequestCorrect = function(reqest) {
+			for (var param in reqest) {
+				return summer.models.Member[param];
+			}
+			//request is empty!!!
+			return false;
+		};
+		if (isRequestCorrect(req.query)) {
+			for (var param in req.query) {
+				var result = database.get(Database.Type.MEMBER, param, req.query[param]);
+			}
+		}
+	});
 
 	//TODO change get to all for all request support (get, post etc)
 	router.all('/create/:name', function(req, res, next) {
